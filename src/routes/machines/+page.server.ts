b4e8bd28fb847c69c86520a4ab33eddbf040c8f5
Stephen <teamgreensf@gmail.com>
2023-04-05 17:29:19 -0700

    Clean up machine man

--- src/routes/machines/+page.server.ts
+++ src/routes/machines/+page.server.ts
@@ -1,5 +1,10 @@
+import type { Database } from "$lib/types/supabase";
 import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types.js";
+import type { PageServerLoad } from "./$types";
+
+export type Machine = Database['public']['Tables']['machines']['Row'] & {
+    machine_def: Database['public']['Tables']['machine_defs']['Row']
+}
 
 export const load = (async ({ locals: { supabase, getSession } }) => {
     const session = await getSession();
@@ -10,10 +15,11 @@ export const load = (async ({ locals: { supabase, getSession } }) => {
         .from('machines')
         .select(`
             *,
-            machine_defs_id (
+            machine_def: machine_defs_id (
                 *
             )
-        `);
+        `)
+        .returns<Machine[]>();
 
     const { data: machineDefs } = await supabase
         .from('machine_defs')
--- src/routes/machines/+page.svelte
+++ src/routes/machines/+page.svelte
@@ -1,20 +1,20 @@
 
 <script lang="ts">
-	import { onMount } from "svelte";
-	import type { PageData } from "./$types.js";
+	import type { PageData } from "./$types";
     import { Bars3, Plus } from 'svelte-heros-v2';
 
     import { offset, flip, shift } from 'svelte-floating-ui/dom';
     import { createFloatingActions } from 'svelte-floating-ui';
     import type { ClientRectObject, VirtualElement } from 'svelte-floating-ui/core';
 	import { writable } from "svelte/store";
+	import type { Machine } from "./+page.server";
 
 
     const [floatingRef, floatingContent] = createFloatingActions({
         strategy: 'absolute',
-        placement: '',
+        placement: "bottom",
         middleware: [
-            offset(-25),
+            offset({ mainAxis: -20}),
             flip(),
             shift()
         ]
@@ -23,19 +23,24 @@
     let x = 0;
     let y = 0;
 
-    function machineRowClicked(event: MouseEvent, machineId: string) {
-        console.log('You clicke on table row with id', machineId);
+    function machineRowClicked(event: MouseEvent, machine: Machine) {
         event.preventDefault();
-        showTooltip = true;
+        contextVisible = true;
+        contextMachine = machine;
         x = event.clientX;
         y = event.clientY;
     }
 
-    const mouseleavecontext = (event: any) => {
-        showTooltip = false;
+    function toggleDeleteModal() {
+        contextVisible = false;
+        deleteModalVisible = true;
     }
 
-    $: getBoundingClientRect = (): ClientRectObject => {
+    const mouseLeaveContext = (event: any) => {
+        contextVisible = false;
+    }
+
+    function getBoundingClientRect(): ClientRectObject {
         return {
             x,
             y,
@@ -47,33 +52,34 @@
             height: 0
         }
     };
-    
-    const virtualElement = writable<VirtualElement>({ getBoundingClientRect, contextElement: document.querySelector('#rightclickcontext') });
-    $: virtualElement.set({ getBoundingClientRect });
+
+    const virtualElement = writable<VirtualElement>({ getBoundingClientRect });
     floatingRef(virtualElement);
 
     export let data: PageData;
     let { session, machines, machineDefs } = data;
 
-    onMount(() => {
+    $: if (machines) {
         console.log(machines);
-    });
+    }
 
-    let showTooltip = false;
+    let contextVisible = false;
+    let contextMachine: Machine | null = null;
 
+    let deleteModalVisible = false;
 </script>
 
 <div class="flex-col space-y-4">
     
-    <div class="flex-row flex justify-between items-end">
+    <div class="flex-row flex justify-start">
         <div class="prose">
-
             <h1 class="inline">Machines</h1>
         </div>
-
+        <div class="divider divider-horizontal"></div>
         <button class="btn btn-primary" tabindex="-1">
-            <Plus tabindex="-1" class="focus:outline-none" />
-        </button> 
+            New Machine
+            <!-- <Plus tabindex="-1" class="focus:outline-none" /> -->
+        </button>
     </div>
     
     <div class="overflow-x-auto">
@@ -81,7 +87,7 @@
             <thead>
                 <tr>
                     <th>Machine ID</th>
-                    <th>Type  <span class="badge badge-info">F. Key</span></th>
+                    <th>Type <span class="badge badge-info">F. Key</span></th>
                     <th>Tier</th>
                     <th>Nickname</th>
                     <th>Actions
@@ -89,9 +95,9 @@
             </thead>
             <tbody>
                 {#each machines || [] as machine}
-                <tr class="hover cursor-pointer" on:click={(event) => machineRowClicked(event, machine.id)}>
+                <tr class="hover cursor-pointer" on:click={(event) => machineRowClicked(event, machine)}>
                     <td><code class="bg-base-200 rounded px-1">{machine.id}</code></td>
-                    <td>{machine.machine_defs_id.make} {machine.machine_defs_id.model}</td>
+                    <td>{machine.machine_def.make} {machine.machine_def.model}</td>
                     <td>{machine.tier}</td>
                     <td>{machine.nickname}</td>
                     <td>
@@ -106,29 +112,28 @@
     </div>
 </div>
 
-
-
-{#if showTooltip}
-<div use:floatingContent on:mouseleave={mouseleavecontext} style="z-index: 99;" id="rightclickmenu">
+{#if contextVisible && contextMachine}
+<div use:floatingContent on:mouseleave={mouseLeaveContext} style="z-index: 99;" id="rightclickmenu">
     <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-    <ul class="menu p-2 shadow bg-base-100 rounded-box w-36">
+    <ul class="menu shadow-lg bg-white rounded-box w-36">
+        <li class="menu-title py-2 select-none"><span>{contextMachine.nickname}</span></li>
         <li><a on:click={() => console.log('lel')}>Edit</a></li>
-        <li><a class="link-warning">Delete</a></li>
+        <li><a class="link-error" on:click={toggleDeleteModal}>Delete</a></li>
     </ul>
 </div>
 {/if}
 
 <!-- The button to open modal -->
-<label for="my-modal" class="btn">open modal</label>
+<label for="delete-machine-modal" class="btn">open modal</label>
 
 <!-- Put this part before </body> tag -->
-<input type="checkbox" id="my-modal" class="modal-toggle" />
-<div class="modal">
+<input type="checkbox" id="delete-machine-modal" class="modal-toggle" bind:checked={deleteModalVisible} />
+<div class="modal lg:ml-80">
   <div class="modal-box">
-    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <h3 class="font-bold text-lg">Delete {contextMachine?.nickname}</h3>
+    <p class="py-4">The delete function is disabled in production.</p>
     <div class="modal-action">
-      <label for="my-modal" class="btn">Yay!</label>
+      <label for="delete-machine-modal" class="btn">Ok</label>
     </div>
   </div>
 </div>
