e110522e2c4c10a7e84447910ad7c362f53c1dcd
Stephen <teamgreensf@gmail.com>
2023-04-05 19:32:40 -0700

    Logout page

---
+++ src/routes/logging-out/+page.server.ts
@@ -0,0 +1,8 @@
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+
+export const load = (async ({ locals: { supabase, getSession } }) => {
+	const session = await getSession();
+
+	return { session };
+}) satisfies PageServerLoad;
---
+++ src/routes/logging-out/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+    import type { PageData } from './$types.js';
+
+    export let data: PageData;
+
+    onMount(async () => {
+        await data.supabase.auth.signOut();
+        goto('/');
+    });
+</script>
+
+<section>
+	"Because as we know, there are known knowns; there are things we know we know. We also know there
+	are known unknowns; that is to say we know there are some things we do not know. But there are
+	also unknown unknowns—the ones we don't know we don't know" - Donald Rumsfeld
+</section>
