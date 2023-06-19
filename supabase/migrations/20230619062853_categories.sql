fdd87d057069a638ec7d44d36995493745c9a028
Stephen <teamgreensf@gmail.com>
2023-06-19 10:00:01 -0700

    Latest migrations

---
+++ supabase/migrations/20230619062853_categories.sql
@@ -0,0 +1,13 @@
+create table
+  public.inv_categories (
+    id uuid not null default uuid_generate_v4 (),
+    name text not null,
+    created_at timestamp with time zone not null default now(),
+    created_by_id uuid not null,
+    constraint inv_categories_pkey primary key (id),
+    constraint inv_categories_created_by_id_fkey foreign key (created_by_id) references profiles (id)
+  ) tablespace pg_default;
+
+alter table public.inv_items 
+  add inv_category_id uuid not null,
+  add constraint inv_items_inv_category_id_fkey foreign key (inv_category_id) references inv_categories (id);
