c46ea6824691892215947e98bf6a09b5f7877454
Stephen <teamgreensf@gmail.com>
2023-10-12 11:02:09 -0700

    Bug fix

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,13 @@
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, PUBLIC_DEV_SUPABASE_ANON_KEY, PUBLIC_DEV_SUPABASE_URL } from '$env/static/public';
 import type { Database } from '$lib/types/supabase.js';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
 import type { Handle } from '@sveltejs/kit';
 import { dev } from '$app/environment';
-import { DEV_SUPABASE_URL, DEV_SUPABASE_ANON_KEY } from '$env/static/private';
 
 export const handle: Handle = async ({ event, resolve }) => {
     event.locals.supabase = createSupabaseServerClient<Database>({
-        supabaseUrl: dev ? DEV_SUPABASE_URL : PUBLIC_SUPABASE_URL,
-        supabaseKey: dev ? DEV_SUPABASE_ANON_KEY : PUBLIC_SUPABASE_ANON_KEY,
+        supabaseUrl: dev ? PUBLIC_DEV_SUPABASE_URL : PUBLIC_SUPABASE_URL,
+        supabaseKey: dev ? PUBLIC_DEV_SUPABASE_ANON_KEY : PUBLIC_SUPABASE_ANON_KEY,
         event
     });
 
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,17 +1,15 @@
-import { invalidate } from '$app/navigation';
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, PUBLIC_DEV_SUPABASE_ANON_KEY, PUBLIC_DEV_SUPABASE_URL } from '$env/static/public';
 import type { Database } from '$lib/types/supabase';
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 import type { LayoutLoad } from './$types';
 import { dev } from '$app/environment';
-import { DEV_SUPABASE_URL, DEV_SUPABASE_ANON_KEY } from '$env/static/private';
 
 export const load: LayoutLoad = async ({ fetch, data, depends }) => {
     depends('supabase:auth');
 
     const supabase = createSupabaseLoadClient<Database>({
-        supabaseUrl: dev ? DEV_SUPABASE_URL : PUBLIC_SUPABASE_URL,
-        supabaseKey: dev ? DEV_SUPABASE_ANON_KEY : PUBLIC_SUPABASE_ANON_KEY,
+        supabaseUrl: dev ? PUBLIC_DEV_SUPABASE_URL : PUBLIC_SUPABASE_URL,
+        supabaseKey: dev ? PUBLIC_DEV_SUPABASE_ANON_KEY : PUBLIC_SUPABASE_ANON_KEY,
         event: { fetch },
         serverSession: data.session
     });
