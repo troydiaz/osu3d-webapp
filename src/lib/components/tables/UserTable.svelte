61a5fe3782d9c2361f5dc11b94496b62b1ccb346
Stephen <teamgreensf@gmail.com>
2023-10-27 09:32:14 -0700

    Responsive user table

--- src/lib/components/tables/UserTable.svelte
+++ src/lib/components/tables/UserTable.svelte
@@ -15,18 +15,22 @@
 </script>
 
 <div class="flex flex-col gap-12">
+  <div class="block lg:hidden grow relative mx-12 lg:mx-0">
+    <input class="input w-full" type="text" placeholder="Search..." bind:value={filterText} />
+    <button class="btn btn-sm btn-ghost hover:bg-transparent absolute right-0 top-2 opacity-50" on:click={() => filterText = ''}>Clear</button>
+  </div>
   <!-- Users table -->
   <div class="flex flex-col gap-4">
-    <div class="flex flex-row justify-start items-center">
+    <div class="flex flex-row justify-start items-center px-4 lg:px-0">
       <div class="font-thin text-3xl">User Accounts</div>
-      <div class="divider divider-horizontal"></div>
-      <div>{userLevels.length} Entries</div>
-      <div class="divider divider-horizontal"></div>
-      <div class="grow relative">
-        <input class="input input-sm w-full" type="text" placeholder="Filter by name or email..." bind:value={filterText} />
+      <div class="hidden lg:flex divider divider-horizontal"></div>
+      <div class="hidden lg:block">{userLevels.length} Entries</div>
+      <div class="hidden lg:flex divider divider-horizontal"></div>
+      <div class="hidden lg:block grow relative">
+        <input class="input input-sm w-full" type="text" placeholder="Search..." bind:value={filterText} />
         <button class="btn btn-sm btn-ghost hover:bg-transparent absolute right-0 opacity-50" on:click={() => filterText = ''}>Clear</button>
       </div>
-      <div class="divider divider-horizontal"></div>
+      <div class="hidden lg:flex divider divider-horizontal"></div>
       <Paginate pageSize={20} totalRows={filteredUsers.length} bind:lowerIndex bind:upperIndex />
     </div>
     {#if lowerIndex !== undefined && upperIndex !== undefined}
@@ -34,18 +38,17 @@
       <table class="table">
         <thead>
           <tr>
-            <th></th>
+            <th class="hidden md:block"></th>
             <th>Full Name</th>
             <th>Email</th>
             <th>Joined On <ChevronDown class="inline" size={'16px'} /></th>
-            <th>Last Update</th>
             <th>Permission Flags</th>
           </tr>
         </thead>
         <tbody>
           {#each filteredUsers.slice(lowerIndex, upperIndex) as user, index}
           <tr class="transition {user.checked ? 'bg-success text-success-content' : ''}">
-            <td class="grid grid-cols-3 gap-1">
+            <td class=" grid-cols-3 gap-1 w-52 hidden md:grid">
                 {#if hasPermission(user.level, PermCategory.TIER_3, PermFlag.FIRST)}
                 <span class="px-1 py-1 font-mono bg-error text-error-content rounded-lg text-xs">TIER 3</span>
                 {/if}
@@ -56,24 +59,21 @@
                 <span class="px-1 py-1 font-mono bg-info text-info-content rounded-lg text-xs">TIER 1</span>
                 {/if}
                 {#if hasPermission(user.level, PermCategory.INVENTORY, PermFlag.FIRST)}
-                <span class="px-1 py-1 font-mono bg-primary text-primary-content rounded-lg text-xs">INV.</span>
+                <span class="px-1 py-1 font-mono bg-primary text-primary-content rounded-lg text-xs">INVEN.</span>
                 {/if}
                 {#if hasPermission(user.level, PermCategory.MAINTENANCE, PermFlag.FIRST)}
                 <span class="px-1 py-1 font-mono bg-primary text-primary-content rounded-lg text-xs">MAINT.</span>
                 {/if}
-
                 {#if hasPermission(user.level, PermCategory.MACHINES, PermFlag.FIRST)}
-                <span class="px-1 py-1 font-mono bg-primary text-primary-content rounded-lg text-xs">MACH.</span>
+                <span class="px-1 py-1 font-mono bg-primary text-primary-content rounded-lg text-xs">PRINT.</span>
                 {/if}
-
                 {#if hasPermission(user.level, PermCategory.SPECIAL, PermFlag.FIRST)}
-                <span class="px-1 py-1 font-mono bg-secondary text-secondary-content rounded-lg text-xs">ADMIN</span>
+                <span class="px-1 py-1 font-mono bg-secondary text-secondary-content rounded-lg text-xs">ADMIN.</span>
                 {/if}
             </td>
             <td>{user.full_name}</td>
             <td>{user.email}</td>
             <td>{getDateAndTime(user.created_at)}</td>
-            <td>{getDateAndTime(user.updated_at)}</td>
             <td>
               <button class="btn btn-sm btn-primary" on:click={() => goto(`/users/${user.user_id}`)} disabled={hasPermission(user.level, PermCategory.SPECIAL, PermFlag.FIRST)}>Edit Flags</button>
             </td>
