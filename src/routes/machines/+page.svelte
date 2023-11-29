7a2e4095c11dc3ed21fd9ef41c356110984dcc0e
Stephen <teamgreensf@gmail.com>
2023-11-28 19:49:02 -0800

    Add tier 1.5 and 2.5 to machines page

--- src/routes/machines/+page.svelte
+++ src/routes/machines/+page.svelte
@@ -17,7 +17,10 @@
     
     {#if machines}
     <MachineTable machines={machines.filter(m => m.tier === 1)} tier={1} />
+    <MachineTable machines={machines.filter(m => m.tier === 99)} tier={1.5} />
     <MachineTable machines={machines.filter(m => m.tier === 2)} tier={2} />
+    <MachineTable machines={machines.filter(m => m.tier === 98)} tier={2.5} />
     <MachineTable machines={machines.filter(m => m.tier === 3)} tier={3} />
+    
     {/if}
 </div>
