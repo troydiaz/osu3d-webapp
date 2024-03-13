225ac95eafd0747fd1502ebe82abcbba898b64ae
Stephen <teamgreensf@gmail.com>
2024-03-12 23:31:56 -0700

    Side nav bar style tweaks

--- src/lib/components/SidebarButton.svelte
+++ src/lib/components/SidebarButton.svelte
@@ -7,12 +7,14 @@
   export let image: string;
 </script>
 
-<SidebarTooltip activeRoute={$page.url.pathname.startsWith(url)}>
-  <li>
-    <a href={url} class="p-0 py-1 group flex-row justify-center items-center bg-gradient-to-b hover:rounded-r-none border border-base-content/10 {$page.url.pathname.startsWith(url) ? 'from-blue-300 to-blue-400 dark:from-blue-400 dark:to-blue-500' : 'hover:from-blue-300 hover:to-blue-400 hover:dark:from-blue-400 hover:dark:to-blue-500'}">
-      <img src={image} alt="Sidebar button" class="w-12 h-12 group-hover:opacity-100 group-hover:scale-110 group-hover:drop-shadow shadow-black transition-all duration-500 ease-in-out {$page.url.pathname.startsWith(url) ? 'drop-shadow rotate-3 opacity-100 scale-110' : 'opacity-50'}" />
-    </a>
-  </li>
-  <div slot="text" class="flex flex-col justify-center items-center h-full">{ name }</div>
-</SidebarTooltip>
+<div class="relative hover:drop-shadow-md shadow-black">
+  <SidebarTooltip activeRoute={$page.url.pathname.startsWith(url)}>
+    <li class="group border border-base-content/5 hover:border-transparent rounded-md transition-all duration-1000 ease-in-out">
+      <a href={url} class="h-[58px] p-0 py-1 flex-row justify-center rounded-md group-hover:rounded-r-none items-center bg-gradient-to-b transition-colors duration-100 {$page.url.pathname.startsWith(url) ? 'from-blue-300 to-blue-400 dark:from-blue-400 dark:to-blue-500' : 'group-hover:from-blue-300 group-hover:to-blue-400 group-hover:dark:from-blue-400 group-hover:dark:to-blue-500'}">
+        <img src={image} alt="Sidebar button" class="w-12 h-12 group-hover:opacity-100 group-hover:scale-110 transition-all duration-500 ease-in-out {$page.url.pathname.startsWith(url) ? 'drop-shadow rotate-3 opacity-100 scale-110' : 'opacity-50'}" />
+      </a>
+    </li>
+    <div slot="text" class="flex flex-col justify-center items-center h-full drop-shadow-lg shadow-black">{ name }</div>
+  </SidebarTooltip>
+</div>
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -46,9 +46,6 @@
   }
 </script>
 
-<div class="fixed top-0 bottom-0 left-0 right-0 bg-gradient-to-bl from-base-100 to-slate-100 dark:from-black dark:to-slate-900 -z-50"></div>
-<!-- <div class="fixed top-0 h-screen w-full -z-10 my-auth-container"></div> -->
-
 <main>
   {#if session}
     <!-- Notification Tray -->
@@ -60,7 +57,7 @@
       <div class="drawer-content lg:px-16 overflow-x-auto">
 
         <!-- Navbar for smaller screens -->
-        <div class="lg:hidden z-20 bg-base-100 navbar fixed bottom-0 justify-between space-x-4 shadow outline outline-1 outline-base-content/10">
+        <div class="lg:hidden z-20 bg-base-100 navbar fixed bottom-0 justify-between space-x-4 shadow border-t border-base-content/10">
           <label for="drawer-id" class="btn btn-square btn-ghost drawer-button lg:hidden"><Bars3 /></label>
           <a href="https://discord.gg/XE2vD4QY">
             <svg height="40px" width="40px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle cx="512" cy="512" r="512" style="fill:#5865f2"></circle> <path d="M689.43 349a422.21 422.21 0 0 0-104.22-32.32 1.58 1.58 0 0 0-1.68.79 294.11 294.11 0 0 0-13 26.66 389.78 389.78 0 0 0-117.05 0 269.75 269.75 0 0 0-13.18-26.66 1.64 1.64 0 0 0-1.68-.79A421 421 0 0 0 334.44 349a1.49 1.49 0 0 0-.69.59c-66.37 99.17-84.55 195.9-75.63 291.41a1.76 1.76 0 0 0 .67 1.2 424.58 424.58 0 0 0 127.85 64.63 1.66 1.66 0 0 0 1.8-.59 303.45 303.45 0 0 0 26.15-42.54 1.62 1.62 0 0 0-.89-2.25 279.6 279.6 0 0 1-39.94-19 1.64 1.64 0 0 1-.16-2.72c2.68-2 5.37-4.1 7.93-6.22a1.58 1.58 0 0 1 1.65-.22c83.79 38.26 174.51 38.26 257.31 0a1.58 1.58 0 0 1 1.68.2c2.56 2.11 5.25 4.23 8 6.24a1.64 1.64 0 0 1-.14 2.72 262.37 262.37 0 0 1-40 19 1.63 1.63 0 0 0-.87 2.28 340.72 340.72 0 0 0 26.13 42.52 1.62 1.62 0 0 0 1.8.61 423.17 423.17 0 0 0 128-64.63 1.64 1.64 0 0 0 .67-1.18c10.68-110.44-17.88-206.38-75.7-291.42a1.3 1.3 0 0 0-.63-.63zM427.09 582.85c-25.23 0-46-23.16-46-51.6s20.38-51.6 46-51.6c25.83 0 46.42 23.36 46 51.6.02 28.44-20.37 51.6-46 51.6zm170.13 0c-25.23 0-46-23.16-46-51.6s20.38-51.6 46-51.6c25.83 0 46.42 23.36 46 51.6.01 28.44-20.17 51.6-46 51.6z" style="fill:#fff"></path> </g></svg>
@@ -75,14 +72,14 @@
         </div>
       </div> 
     
-      <div class="z-20 drawer-side overflow-visible">
+      <div class="z-20 drawer-side overflow-visible border-r border-base-content/10">
         <label for="drawer-id" class="drawer-overlay"></label>
         <ul class="menu h-full px-4 bg-base-100 outline outline-1 outline-base-content/10">
-          <div class="h-full flex flex-col justify-start items-center gap-4 py-4">
+          <div class="flex flex-col h-full justify-start items-center gap-4 py-4">
             <div class="w-12">
               <img src="/osu3d.svg" alt="Club logo" class="m-auto opacity-75">
             </div>
-            <div class="h-full flex flex-col justify-between mt-2">
+            <div class="flex flex-col h-full justify-between mt-2">
               <div class="flex flex-col justify-start md:gap-3 gap-2">
                 <!-- Dashboard -->
                 <SidebarButton name="Dashboard" url="/dashboard" image={log} />
@@ -111,25 +108,29 @@
               </div>
         
               <div class="flex flex-col justify-start gap-2">
-                <SidebarTooltip>
-                  <div slot="text" class="flex flex-col justify-center items-center h-full">Theme</div>
-                  <li>
-                    <button data-toggle-theme="light,dark" data-act-class="swap-active" class="py-1 group swap bg-gradient-to-b hover:rounded-r-none border border-base-content/10 hover:from-blue-300 hover:to-blue-400 hover:dark:from-blue-400 hover:dark:to-blue-500">
-                      <div class="swap-off"><img src={sun} alt="Enter dark mode" class="opacity-50 w-12 h-12 group-hover:opacity-100 group-hover:scale-110 group-hover:drop-shadow shadow-black transition-all duration-500 ease-in-out" /></div>
-                      <div class="swap-on"><img src={flashlight} alt="Enter light mode" class="opacity-50 w-12 h-12 group-hover:opacity-100 group-hover:scale-110 group-hover:drop-shadow shadow-black transition-all duration-500 ease-in-out" /></div>
-                    </button>
-                  </li>
-                </SidebarTooltip>
+                <div class="relative hover:drop-shadow-md shadow-black">
+                  <SidebarTooltip>
+                    <li class="group border border-base-content/5 hover:border-transparent rounded-md transition-all duration-1000 ease-in-out">
+                      <button data-toggle-theme="light,dark" data-act-class="swap-active" class="disable-focus h-[58px] py-1 group swap rounded-md group-hover:rounded-r-none transition-colors duration-100 bg-gradient-to-b group-hover:from-blue-300 group-hover:to-blue-400 group-hover:dark:from-blue-400 group-hover:dark:to-blue-500">
+                        <div class="swap-off"><img src={sun} alt="Enter dark mode" class="opacity-50 w-12 h-12 group-hover:opacity-100 group-hover:scale-110 group-hover:drop-shadow shadow-black transition-all duration-500 ease-in-out" /></div>
+                        <div class="swap-on"><img src={flashlight} alt="Enter light mode" class="opacity-50 w-12 h-12 group-hover:opacity-100 group-hover:scale-110 group-hover:drop-shadow shadow-black transition-all duration-500 ease-in-out" /></div>
+                      </button>
+                    </li>
+                    <div slot="text" class="flex flex-col justify-center items-center h-full drop-shadow-lg shadow-black">Theme</div>
+                  </SidebarTooltip>
+                </div>
         
                 <!-- Logout Button -->
-                <SidebarTooltip>
-                  <div slot="text" class="flex flex-col justify-center items-center h-full">Logout</div>
-                  <li>
-                    <button on:click={() => signOut()} class="py-1 group swap bg-gradient-to-b hover:rounded-r-none border border-base-content/10 hover:from-blue-300 hover:to-blue-400 hover:dark:from-blue-400 hover:dark:to-blue-500">
-                      <img src={key} alt="Logout" class="opacity-50 w-12 h-12 group-hover:opacity-100 group-hover:scale-110 group-hover:drop-shadow shadow-black transition-all duration-500 ease-in-out" />
-                    </button>
-                  </li>
-                </SidebarTooltip>
+                <div class="relative hover:drop-shadow-md shadow-black">
+                  <SidebarTooltip>
+                    <li class="group border border-base-content/5 hover:border-transparent rounded-md transition-all duration-1000 ease-in-out">
+                      <button on:click={() => signOut()} class="disable-focus h-[58px] py-1 group swap bg-gradient-to-b rounded-md group-hover:rounded-r-none transition-colors duration-100 group-hover:from-blue-300 group-hover:to-blue-400 group-hover:dark:from-blue-400 group-hover:dark:to-blue-500">
+                        <img src={key} alt="Logout" class="opacity-50 w-12 h-12 group-hover:opacity-100 group-hover:scale-110 group-hover:drop-shadow shadow-black transition-all duration-500 ease-in-out" />
+                      </button>
+                    </li>
+                    <div slot="text" class="flex flex-col justify-center items-center h-full drop-shadow-lg shadow-black">Logout</div>
+                  </SidebarTooltip>
+                </div>
               </div>
             </div>
           </div>
@@ -142,3 +143,6 @@
 </main>
 
 <Toaster position="top-right" />
+
+<div class="fixed top-0 bottom-0 left-0 right-0 bg-gradient-to-bl from-base-100 to-slate-100 dark:from-black dark:to-slate-900 -z-10 pointer-events-none select-none"></div>
+<!-- <div class="fixed top-0 h-screen w-full -z-10 my-auth-container"></div> -->
