ed819503de5cd8e52d0db5ee1ddcfd2af98b1758
Troy Diaz <troyk1718@gmail.com>
2025-06-23 22:51:11 -1000

    Fixing UI issues with themes.

--- src/routes/account/ThemePreview.svelte
+++ src/routes/account/ThemePreview.svelte
@@ -3,13 +3,21 @@
 </script>
 
 {#if theme}
-  <div class="border-base-content/20 hover:border-base-content/40 overflow-hidden rounded-lg border outline outline-2 outline-offset-2 outline-transparent" data-set-theme={theme} data-act-class="!outline-base-content">
-    <div data-theme={theme} class="bg-base-100 text-base-content w-full cursor-pointer font-sans">
+  <div
+    class="border-base-content/20 hover:border-base-content/40
+           overflow-hidden rounded-lg border
+           outline outline-2 outline-offset-2 outline-transparent"
+    data-theme={theme}
+    data-set-theme={theme}
+    data-act-class="!outline-base-content"
+  >
+    <div class="bg-base-100 text-base-content w-full cursor-pointer font-sans">
       <div class="grid grid-cols-5 grid-rows-3">
         <div class="bg-base-200 col-start-1 row-span-2 row-start-1"></div> 
         <div class="bg-base-300 col-start-1 row-start-3"></div> 
-        <div class="bg-base-100 col-span-4 col-start-2 row-span-3 row-start-1 flex flex-col gap-1 p-2">
-          <div class="font-bold">{ theme }</div>
+        <div class="bg-base-100 col-span-4 col-start-2 row-span-3 row-start-1
+                    flex flex-col gap-1 p-2">
+          <div class="font-bold capitalize">{theme}</div>
           <div class="flex flex-wrap gap-1">
             <div class="bg-primary flex aspect-square w-5 items-center justify-center rounded lg:w-6">
               <div class="text-primary-content text-sm font-bold">A</div>
